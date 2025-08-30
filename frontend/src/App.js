ac66629a115d4f9a6e9679d31ca699f189e1d466
Piyush-Sawlani <psawlani153@gmail.com>
2025-08-30 12:55:41 +0530

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Register from './Components/QrPage';
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
